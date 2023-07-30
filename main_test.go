04d1b3c3cce4959c2b55bbedda73ee478428c26d
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-07-30 16:13:47 +0900

    失敗パターン

--- main_test.go
+++ main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestAdd(t *testing.T) {
-	total := Add(1, 5)
+	total := Add(1, 4)
 	if total != 6 {
 		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 6)
 	}
